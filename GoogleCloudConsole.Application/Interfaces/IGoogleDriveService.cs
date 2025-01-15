ab5fabbc43c99582d0b56ce7ce59bc7cb5d282f4
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-01-15 16:02:17 +0300

    feat(interface): interface for GoogleCloudConsole has been added

---
+++ GoogleCloudConsole.Application/Interfaces/IGoogleDriveService.cs
@@ -0,0 +1,10 @@
+﻿using CSharpFunctionalExtensions;
+using GoogleCloudConsole.Domain.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace GoogleCloudConsole.Application.Interfaces;
+
+public interface IGoogleDriveService
+{
+    Task<Result<Image>> CreateImage(IFormFile file, string fileName);
+}
