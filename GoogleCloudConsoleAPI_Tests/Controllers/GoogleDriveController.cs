959dac658f3d9f52bf14fb2a8f0389ab819107c7
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-01-15 16:25:21 +0300

    feat(controler): controller for downloading a photo to the Google Drive has added

---
+++ GoogleCloudConsoleAPI_Tests/Controllers/GoogleDriveController.cs
@@ -0,0 +1,34 @@
+﻿using GoogleCloudConsole.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoogleCloudConsoleAPI_Tests.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class GoogleDriveController : ControllerBase
+{
+    private readonly IGoogleDriveService _googleDriveService;
+
+    public GoogleDriveController(IGoogleDriveService googleDriveService)
+    {
+        _googleDriveService = googleDriveService;
+    }
+
+    [HttpPost("upload")]
+    public async Task<IActionResult> UploadImage([FromForm] IFormFile file)
+    {
+        if (file == null || file.Length == 0)
+        {
+            return BadRequest("Invalid file.");
+        }
+
+        var result = await _googleDriveService.CreateImage(file, file.FileName);
+
+        if (result.IsFailure)
+        {
+            return BadRequest(result.Error);
+        }
+
+        return Ok(new { result.Value.FileName, result.Value.Url });
+    }
+}
