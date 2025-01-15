bf66123f7a46b77245036a91d3d8540c66c192d4
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-01-15 16:04:44 +0300

    feat(class): a DI class has been added

---
+++ GoogleCloudConsoleAPI_Tests/Extensions/ServiceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using GoogleCloudConsole.Application.Interfaces;
+using GoogleCloudConsole.Infrastructure.Services;
+
+namespace GoogleCloudConsoleAPI_Tests.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddTransient<IGoogleDriveService, GoogleDriveService>();
+    }
+}
